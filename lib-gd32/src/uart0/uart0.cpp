0b2c4db574219e84eec10dfc55df62c5acc448c2
Arjan <Arjan.van.Vught@gmail.com>
2026-09-24 10:16:01 +0200

    Add DMA16 helper and fix build issues
    Introduce a GD32F4 16-bit DMA memcpy helper and add related MCU/DMA definitions, including optional PA8 ENET clock output disabling. The rest of the change is a broad portability and correctness cleanup: normalize preprocessor guards, switch several call sites to standard min/max utilities, and fix small but important bugs in SPI chip select handling, UART transmission, HTTP request parsing, and software timer debug flow.

--- lib-gd32/src/uart0/uart0.cpp
+++ lib-gd32/src/uart0/uart0.cpp
@@ -23,9 +23,6 @@
  * THE SOFTWARE.
  */
 
-// #define CONFIG_USART0_ENABLE_RX_DMA
-// #define CONFIG_USART0_ENABLE_TX_DMA
-
 #include <cstdint>
 #include <cstdio>
 #include <cstdarg>
@@ -33,18 +30,18 @@
 #include "gd32_uart.h"
 #if defined(CONFIG_USART0_ENABLE_TX_DMA) || defined(CONFIG_USART0_ENABLE_RX_DMA)
 #include "gd32_dma.h"
-#endif // defined(CONFIG_USART0_ENABLE_TX_DMA) || defined(CONFIG_USART0_ENABLE_RX_DMA)
+#endif            // defined(CONFIG_USART0_ENABLE_TX_DMA) || defined(CONFIG_USART0_ENABLE_RX_DMA)
 #include "gd32.h" // IWYU pragma: keep
 
 namespace uart0 {
 static char s_printf_buffer[128];
 
-#if defined(CONFIG_USART0_ENABLE_TX_DMA)
+#ifdef CONFIG_USART0_ENABLE_TX_DMA
 // static char s_tx_buffer[128];
 // static constexpr uint32_t kSizeTxBuffer = sizeof(s_tx_buffer);
 #endif // CONFIG_USART0_ENABLE_TX_DMA
 
-#if defined(CONFIG_USART0_ENABLE_RX_DMA)
+#ifdef CONFIG_USART0_ENABLE_RX_DMA
 static char s_rx_buffer[128];
 struct RxCount {
     uint16_t rx;
@@ -75,17 +72,17 @@ void Init() {
     gd32::UartBegin(USART0, 115200U, gd32::kUartBits8, gd32::kUartParityNone, gd32::kUartStop1Bit);
 #if defined(CONFIG_USART0_ENABLE_TX_DMA) || defined(CONFIG_USART0_ENABLE_RX_DMA)
     // DMA
-#if defined(GD32H7XX)
+#ifdef GD32H7XX
     rcu_periph_clock_enable(RCU_DMAMUX);
 #endif // GD32H7XX
 
     rcu_periph_clock_enable(USART0_RCU_DMAx);
 
     DMA_PARAMETER_STRUCT dma_init_struct;
-#if defined(CONFIG_USART0_ENABLE_TX_DMA)
+#ifdef CONFIG_USART0_ENABLE_TX_DMA
     dma_deinit(USART0_DMAx, USART0_TX_DMA_CHx);
     dma_struct_para_init(&dma_init_struct);
-#if defined(GD32H7XX)
+#ifdef GD32H7XX
     dma_init_struct.request = DMA_REQUEST_USART0_TX;
 #endif // GD32H7XX
     dma_deinit(USART0_DMAx, USART0_TX_DMA_CHx);
@@ -106,14 +103,14 @@ void Init() {
     dma_init_struct.priority = DMA_PRIORITY_LOW;
     dma_init(USART0_DMAx, USART0_TX_DMA_CHx, &dma_init_struct);
     dma_circulation_disable(USART0_DMAx, USART0_TX_DMA_CHx);
-#if defined(GD32F4XX)
+#ifdef GD32F4XX
     dma_channel_subperipheral_select(USART0_DMAx, USART0_TX_DMA_CHx, USART0_TX_DMA_SUBPERIx);
 #endif // GD32F4XX
     // USART
     usart_dma_transmit_config(USART0, USART_TRANSMIT_DMA_ENABLE);
 #endif // CONFIG_USART0_ENABLE_TX_DMA
 
-#if defined(CONFIG_USART0_ENABLE_RX_DMA)
+#ifdef CONFIG_USART0_ENABLE_RX_DMA
     dma_deinit(USART0_DMAx, USART0_RX_DMA_CHx);
     dma_struct_para_init(&dma_init_struct);
 #if defined(GD32H7XX)
@@ -155,7 +152,7 @@ void Init() {
 #endif // defined(CONFIG_USART0_ENABLE_TX_DMA) || defined(CONFIG_USART0_ENABLE_RX_DMA)
 }
 
-#if defined(CONFIG_USART0_ENABLE_TX_DMA)
+#ifdef CONFIG_USART0_ENABLE_TX_DMA
 void WriteDma(const void* data, uint32_t size) {
     assert(data != nullptr);
     assert(size <= DMA_CHXCNT_CNT);
@@ -212,7 +209,7 @@ void Puts(const char* string) {
     PutChar('\n');
 }
 
-#if defined(CONFIG_USART0_ENABLE_RX_DMA)
+#ifdef CONFIG_USART0_ENABLE_RX_DMA
 int GetChar() {
     if (sv_receive_flag != 1) [[unlikely]] {
         return EOF;
@@ -232,13 +229,13 @@ int GetChar() {
 }
 #else
 int GetChar() {
-    if (__builtin_expect((!gd32::UartFlagGet<USART_FLAG_RBNE>(USART0)), 1)) {
+    if (!gd32::UartFlagGet<USART_FLAG_RBNE>(USART0)) [[likely]] {
         return EOF;
     }
 
     const auto kChar = static_cast<int>(USART_RDATA(USART0));
 
-#if defined(UART0_ECHO)
+#ifdef UART0_ECHO
     PutChar(kChar);
 #endif // UART0_ECHO
 
